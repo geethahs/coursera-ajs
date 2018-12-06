86b51358bafb2f7ade405915088fa8a2dd7ee5ae
geethahs <geethahs210@gmail.com>
2018-12-06 15:50:59 +0530

    correction

--- module2-solution/js/app.js
+++ module2-solution/js/app.js
@@ -6,19 +6,121 @@ var itemsbought =[];
 angular.module('ShoppingListApp', [])
 .controller('ToBuyShoppingListController', ToBuyShoppingListController)
 .controller('BoughtShoppingListController', BoughtShoppingListController)
+.provider('ShoppingListService', ShoppingListServiceProvider);
+
+ToBuyShoppingListController.$inject = ['ShoppingListService'];
+function ToBuyShoppingListController(ShoppingListService) {
+
+  var showList = this;
+  showList.items = ShoppingListService.getItems();
+  showList.errorBuyMessage = "";
+
+  showList.removeItem = function (itemIndex) {
+  	ShoppingListService.moveItem(itemIndex);
+    try{
+    ShoppingListService.removeItem(itemIndex);
+}
+catch (error) {
+      showList.errorBuyMessage = error.message;
+    }
+  };
+}
+
+BoughtShoppingListController.$inject = ['ShoppingListService'];
+function BoughtShoppingListController(ShoppingListService) {
+
+  var showMovedList = this;
+  var objreturn = ShoppingListService.getMovedItems();
+  showMovedList.items = objreturn.items;
+  showMovedList.errorBoughtMessage = objreturn.message;
+
+}
+
+
+function ShoppingListService(defaults) {
+  var service = this;
+  var items = [{
+			    name: "cookies",
+			    quantity: "10"
+			},
+			{
+			    name: "cookies",
+			    quantity: "10"
+			},
+			{
+			    name: "cookies",
+			    quantity: "10"
+			}
+			];
+
+  service.moveItem = function (itemIndex) {
+    itemsbought.push(items[itemIndex]);
+  };
+
+  service.removeItem = function (itemIndex) {
+    items.splice(itemIndex, 1);
+    if(!items.length){
+    	throw new Error(defaults.errorBuyMessage);
+    }
+  };
+
+  service.getItems = function () {
+      return items;
+  };
+
+  service.getMovedItems = function () {
+    var obj = {
+      items : itemsbought,
+      message : defaults.errorBoughtMessage
+  };
+    return obj;
+  };
+}
+
+function ShoppingListServiceProvider() {
+  var provider = this;
+
+  provider.defaults = {
+    errorBuyMessage : "Everything is bought!",
+    errorBoughtMessage : "Nothing bought yet."
+  };
+
+  provider.$get = function () {
+    var shoppingList = new ShoppingListService(provider.defaults);
+
+    return shoppingList;
+  };
+}
+
+})();
+
+
+
+
+
+/*
+(function () {
+'use strict';
+
+var itemsbought =[];
+
+angular.module('ShoppingListApp', [])
+.controller('ToBuyShoppingListController', ToBuyShoppingListController)
+.controller('BoughtShoppingListController', BoughtShoppingListController)
 .factory('ShoppingListFactory', ShoppingListFactory);
+
 
 ToBuyShoppingListController.$inject = ['ShoppingListFactory'];
 function ToBuyShoppingListController(ShoppingListFactory) {
 
-  var showList = this;  
+  var showList = this;
   var shoppingList = ShoppingListFactory();
   showList.items = shoppingList.getItems();
   showList.errorBuyMessage = "";
 
   showList.removeItem = function (itemIndex) {
   	shoppingList.moveItem(itemIndex);
-    try{  
+    try{
     shoppingList.removeItem(itemIndex);
 }
 catch (error) {
@@ -30,7 +132,7 @@ catch (error) {
 BoughtShoppingListController.$inject = ['ShoppingListFactory'];
 function BoughtShoppingListController(ShoppingListFactory) {
 
-  var showMovedList = this;  
+  var showMovedList = this;
   var shoppingList = ShoppingListFactory();
   showMovedList.items = shoppingList.getMovedItems();
   showMovedList.errorBoughtMessage = "Nothing bought yet.";
@@ -58,7 +160,7 @@ function ShoppingListService() {
     itemsbought.push(items[itemIndex]);
   };
 
-  service.removeItem = function (itemIndex) {  	
+  service.removeItem = function (itemIndex) {
     items.splice(itemIndex, 1);
     if(!items.length){
     	throw new Error("Everything is bought!");
@@ -66,7 +168,7 @@ function ShoppingListService() {
   };
 
   service.getItems = function () {
-      return items;  	
+      return items;
   };
 
   service.getMovedItems = function () {
@@ -85,60 +187,5 @@ function ShoppingListFactory() {
 })();
 
 
-
-
-
-/*
-(function (){
-	'use strict';
-
-	
-
-	angular.module('ShoppingListApp',[])
-	.controller('ToBuyController',ToBuyController)
-	.factory('ShoppingListFactory', ShoppingListFactory);
-
-	ToBuyController.$inject = ['ShoppingListFactory'];
-
-	function ToBuyController(ShoppingListFactory){
-		var list = this;
-
-	  // Use factory to create new shopping list service
-	  var shoppingList = ShoppingListFactory();
-
-	  list.items = shoppingList.getItems();
-	}
-
-	function ShoppingListService(){
-		var service = this;
-		if(shoppingList.length <= 0){
-			return "Bought all items";
-		}
-		else{
-			var shoppingList = [
-			  {
-			    name: "cookies",
-			    quantity: "10"
-			  },
-			  {
-			    name: "cookies",
-			    quantity: "10"
-			  }
-			];
-			service.getItems = function () {
-			    return shoppingList;
-			};
-		}
-	}
-
-	function ShoppingListFactory() {
-	  	var factory = function () {
-	    return new ShoppingListService();
-	  };
-
-	  return factory;
-	}
-
-})();
 
 */
